0ac69479bc80512f5c463c47c4523c116d78b298
rzartur <artur.rzycki@gmail.com>
2026-02-18 17:38:57 +0100

    Define port environment variable

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
